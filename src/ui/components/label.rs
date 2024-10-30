aa9a3895e3c87c81a91669947c0e65d56fe45fd7
José Quadrado <joseqcorreia@gmail.com>
2024-10-30 00:36:06 +0000

    Add default font to component label.

--- src/ui/components/label.rs
+++ src/ui/components/label.rs
@@ -1,9 +1,4 @@
-use std::{ops::Rem, usize};
-
-use sdl2::{
-    pixels::{Color, PixelFormatEnum},
-    rect::Rect,
-};
+use sdl2::{pixels::Color, rect::Rect};
 
 use crate::utils::draw::draw_string_texture;
 
@@ -12,11 +7,16 @@ use super::traits::{EventConsumer, Render, UIComponent};
 pub struct Label {
     pub id: String,
     pub text: String,
+    pub font_name: Option<String>,
 }
 
 impl Label {
     pub fn new(id: String, text: String) -> Label {
-        Label { id, text }
+        Label {
+            id,
+            text,
+            font_name: None,
+        }
     }
 }
 
@@ -30,11 +30,12 @@ impl Render for Label {
         cache: &mut crate::utils::cache::TextureCache,
         _app: &crate::app::App,
         canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
-        rect: Rect,
-        elapsed: u128,
+        _rect: Rect,
+        _elapsed: u128,
     ) {
-        let font = cache.fonts.get_font("normal-24");
-        let (_fw, fh) = font.size_of(" ").unwrap();
+        let font = cache
+            .fonts
+            .get_font(self.font_name.clone().unwrap_or("normal-24".to_string()));
         let color = Color::RGBA(100, 100, 100, 255);
         let texture = draw_string_texture(self.text.clone(), texture_creator, font, color);
         let (w, h) = (texture.query().width, texture.query().height);
@@ -55,8 +56,4 @@ impl UIComponent for Label {
     fn set_state(&mut self, state: Box<dyn std::any::Any>) {
         self.text = state.downcast_ref::<String>().unwrap().to_string();
     }
-
-    // fn set_state(&mut self, state: Box<dyn std::any::Any>) {
-    //     self.items = state.downcast_ref::<Vec<String>>().unwrap().to_vec();
-    // }
 }
--- src/ui/components/mod.rs
+++ src/ui/components/mod.rs
@@ -1,3 +1,4 @@
+pub mod clock;
 pub mod label;
 pub mod list;
 pub mod spinner;
